7e761c7b2cc409a5f7049330136ea28bc931b86c
heysep <sdvakili@gmail.com>
2015-10-30 13:26:14 -0400

    Refactor the current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    user_id = session[:user_id]
-    user_id && User.find(user_id)
+    session[:user_id] && User.find(user_id)
   end
 
   def authenticate_user!
