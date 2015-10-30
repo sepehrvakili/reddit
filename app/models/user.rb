d1a7efc865a568a0037752b01b632dca1fbb1294
Daniel <dbarton8406@gmail.com>
2015-10-30 11:53:09 -0400

    added a registrations controller/view, also updated modules on post.rb,user.rb,comment.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :posts
+
+
+  validates_length_of :password, minimum: 10
+  validates_uniqueness_of :email
+  validates_presence_of :first_name, :last_name, :email, :password
 end
