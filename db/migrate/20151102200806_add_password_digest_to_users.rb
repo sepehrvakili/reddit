2e22455d0f85850d6b1f6d5ad2a815d620a00a0e
heysep <sdvakili@gmail.com>
2015-11-02 15:23:33 -0500

    Add password digest field to users model

---
+++ db/migrate/20151102200806_add_password_digest_to_users.rb
@@ -0,0 +1,10 @@
+class AddPasswordDigestToUsers < ActiveRecord::Migration
+  def change
+  	add_column :users, :password_digest, :string
+  	User.find_each do |u|
+  		u.password = u[:password]
+  		u.save!
+  	end
+  	remove_column :users, :password, :string
+  end
+end
