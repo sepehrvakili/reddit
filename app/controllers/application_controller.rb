5b692432f74d5ef622bd6688e2d27bb8abb77511
heysep <sdvakili@gmail.com>
2015-11-22 13:55:05 -0500

    Add devise config instead of manual authetication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_user
-    user_id = session[:user_id]
+  protected
 
-    if user_id
-      @current_user ||= User.find(user_id)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
-  def authenticate_user!
-    unless current_user 
-      flash[:notice] = "You have to be logged in to do that."
-      redirect_to login_path
-    end
-  end
+  ## Replacing the old manual way with Devise above
+  # helper_method :current_user
+
+  # def current_user
+  #   user_id = session[:user_id]
+
+  #   if user_id
+  #     @current_user ||= User.find(user_id)
+  #   end
+  # end
+
+  # def authenticate_user!
+  #   unless current_user 
+  #     flash[:notice] = "You have to be logged in to do that."
+  #     redirect_to login_path
+  #   end
+  # end
   
 end
