428db6ec4aae222b9125a49511518841b3528699
heysep <sdvakili@gmail.com>
2015-11-22 17:46:51 -0500

    Modify views and add username option

---
+++ db/migrate/20151122221027_add_username_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsernameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    change_column :users, :username, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122184554) do
+ActiveRecord::Schema.define(version: 20151122221027) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content",    null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151122184554) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username",                            null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
