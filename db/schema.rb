f08f03f19d8558acd35b7f9a0d007388f0b2614f
heysep <sdvakili@gmail.com>
2015-11-22 17:06:51 -0500

    Remove css sheets for registrations and session

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 20151122184554) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts_voters", force: :cascade do |t|
-    t.integer  "post_id",    null: false
-    t.integer  "user_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                               null: false
     t.datetime "created_at",                          null: false
